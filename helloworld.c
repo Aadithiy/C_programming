89a5adcae0ad8cca341c6ea642db0b295c280465
Adithya <adithya200528@gmail.com>
2026-07-24 08:40:12 +0000

    helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,9 @@
+#include <stdio.h>  // Standard I/O library
+
+int main() {
+    // Print message to the console
+    printf("Hello, World!\n");
+    
+    return 0; // Indicate successful program termination
+}
+
