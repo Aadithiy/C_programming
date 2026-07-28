e923c0845a9186454dd172bfe79609c9e4634a62
Adithya <adithya200528@gmail.com>
2026-07-28 06:25:38 +0000

    even_or_odd.c

---
+++ array/even_or_odd.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+	int arr[10],i,n,even=0,odd=0;
+	printf("Enter the number of elements:");
+	scanf("%d",&n);
+	printf("Enter %d elements:\n",n);
+	for(i=0;i<n;i++){
+		scanf("%d", &arr[i]);
+	}
+	for(i=0;i<n;i++){
+		if(arr[i]%2==0)
+			even++;
+		
+		else
+			odd++;
+		
+	}
+        printf("The even number are %d\n",even);
+        printf("The odd numbers are %d\n",odd);
+}
+
+
