af854f0a45bfd0f8a2d9aa7a89517a84089ca358
Aadithiy <adithya200528@gmail.com>
2026-08-15 15:40:21 +0000

    reverse_array.c

---
+++ Function/reverse_array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void reverse(int arr[],int n)
+{
+	int i,temp;
+	for (i=0;i<n/2;i++)
+	{
+		temp=arr[i];
+		arr[i]=arr[n-i-1];
+		arr[n-i-1]=temp;
+	}
+}
+int main()
+{
+	int arr[100],n,i;
+	printf("Enter number of elements:");
+	scanf("%d",&n);
+	printf("Enter the elements:");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	reverse(arr,n);
+	printf("Reversed elements:");
+	for(i=0;i<n;i++)
+	{
+		printf("%d\t",arr[i]);
+	}
+	return 0;
+}
