944989ccc0bab5780f975d544789ab5d044b2abd
Aadithiy <adithya200528@gmail.com>
2026-08-15 17:54:48 +0000

    counnt.c

---
+++ Function/count.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+void countCharacters(char str[])
+{
+    int i;
+    int vowels = 0;
+    int consonants = 0;
+    int digits = 0;
+    int spaces = 0;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' ||
+            str[i] == 'o' || str[i] == 'u' ||
+            str[i] == 'A' || str[i] == 'E' || str[i] == 'I' ||
+            str[i] == 'O' || str[i] == 'U')
+        {
+            vowels++;
+        }
+        else if ((str[i] >= 'a' && str[i] <= 'z') ||
+                 (str[i] >= 'A' && str[i] <= 'Z'))
+        {
+            consonants++;
+        }
+        else if (str[i] >= '0' && str[i] <= '9')
+        {
+            digits++;
+        }
+        else if (str[i] == ' ')
+        {
+            spaces++;
+        }
+    }
+
+    printf("Vowels = %d\n", vowels);
+    printf("Consonants = %d\n", consonants);
+    printf("Digits = %d\n", digits);
+    printf("Spaces = %d\n", spaces);
+}
+
+int main()
+{
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    countCharacters(str);
+
+    return 0;
+}
