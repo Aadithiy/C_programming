b0a50ecfc66fbed903c40dfed793965e2d0a16c7
Aadithiy <adithya200528@gmail.com>
2026-08-31 11:22:31 +0000

    left_half_pyramid.c

---
+++ pattern/left_half_pyramid.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+	int row=5;
+	for(int i=0;i<row;i++)
+	{
+		for(int j=0;j<(row-i)-1;j++)
+		{
+			printf(" ");
+		}
+		for(int j=0;j<=i;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
